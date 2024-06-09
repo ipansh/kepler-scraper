7d13ac2ae982c73f5f15d2a2f4a6160754242c45
Ilya Panshenskov <ipansh@gmail.com>
2024-06-09 15:54:14 +0200

    revert

--- src/scraper.py
+++ src/scraper.py
@@ -22,8 +22,7 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 service = Service(os.environ.get("CHROMEDRIVER_PATH"))
 selenium_driver = webdriver.Chrome(service=service,
-                                   options=chrome_options,
-                                   port=59939)
+                                   options=chrome_options)
 
 
 print(selenium_driver)
