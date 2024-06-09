d457196fae7ce0a2d2f776994f1acdc6effd79bd
Ilya Panshenskov <ipansh@gmail.com>
2024-06-09 14:35:15 +0200

    update webdriver

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 
 from google.cloud import storage
@@ -16,7 +17,10 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-selenium_driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+service = Service(os.environ.get("CHROMEDRIVER_PATH"))
+options = webdriver.ChromeOptions()
+selenium_driver = webdriver.Chrome(service=service, options=options)
 
 app = FastAPI()
 app.include_router(scraper.router)
