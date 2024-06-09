bb838e436312f1843b1c2222b546ea4801b8c9a2
Ilya Panshenskov <ipansh@gmail.com>
2024-06-09 14:11:45 +0200

    import packages

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from fastapi import FastAPI
 import uvicorn
 import os
 
+
+from selenium import webdriver
+import pandas as pd
+
+
+from google.cloud import storage
+import time
+import datetime
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+selenium_driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+spreadsheet_key = os.environ.get("SPREADSHEET_KEY")
+telegram_token = os.environ.get("TELEGRAM_TOKEN")
+
 app = FastAPI()
 app.include_router(scraper.router)
 
