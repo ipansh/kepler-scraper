00b40634f8ada5972ce4e3fa4dcddaf670940434
Ilya Panshenskov <ipansh@gmail.com>
2024-06-19 11:42:36 +0200

    add port

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,8 @@
 import multiprocessing
+import os
 
-bind = "0.0.0.0:$PORT"         # Bind to the port assigned by Heroku
+port = int(os.environ.get("PORT", 8000))  # Default to 8000 if not specified by Heroku
+bind = f"0.0.0.0:{port}"   # Bind to the Heroku-assigned port
 workers = multiprocessing.cpu_count() * 2 + 1  # Set the number of worker processes 
 worker_class = "uvicorn.workers.UvicornWorker"  # Use Uvicorn as the worker class
 timeout = 60
